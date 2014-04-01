f7993292a7095ca2f0d50f1f410f78cec4176a23
Chris <christopher_hauss@gmx.de>
2014-04-01 23:07:42 +0200

    added tokenSetBuilder

---
+++ modules/controller/tokenSetBuilder.py
@@ -0,0 +1,25 @@
+'''
+Created on 01.04.2014
+
+@author: Chris
+'''
+from modules.model.token import MAX_RANK
+import logging.config
+
+logging.config.fileConfig('C:\\Users\\Chris\\git\\stratgame\\config\\log.config')
+logger = logging.getLogger('controller')
+
+def buildDefaultTokenSet(gameData):
+    '''
+    builds a list of token ranks that is used for
+    the placing of the tokens in the beginning
+    '''
+    tokenSet = []
+    for x in range(MAX_RANK):
+        tokenSet.append(0)
+    
+    for x in range(gameData.tokensPerPlayer()):
+        tokenSet[x % MAX_RANK] += 1
+        
+    return tokenSet
+    
