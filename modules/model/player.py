8bef5b592c471ba5fedea2be372c9d6a0163cc13
Chris <christopher_hauss@gmx.de>
2014-04-01 23:08:10 +0200

    added a list of tokens on the field of a player to the player

--- modules/model/player.py
+++ modules/model/player.py
@@ -16,6 +16,7 @@ class Player():
         '''
         self.__name = name
         self.__index = index
+        self.__tokensOnField = []
         logger.debug("Created new player with name=%s" % name)
         
     def getName(self):
@@ -29,6 +30,22 @@ class Player():
         returns the index of the player
         '''
         return self.__index
+    
+    def addTokenOnField(self, token):
+        '''
+        adds a token to the players tokens on the field
+        '''
+        self.__tokensOnField.append(token)
+        
+    def removeTokenOnField(self, token):
+        '''
+        removes a token of the player from the list of its
+        tokens on the field
+        '''
+        if token in self.__tokensOnField:
+            self.__tokensOnField.remove(token)
+        else:
+            raise ValueError("Token is not in players list: can not remove it")
         
     def __str__(self):
         return "%s" % self.__name
