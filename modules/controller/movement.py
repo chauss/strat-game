d710c6951a5aa30fd18491f4f1e4e8cba1ea76b2
Chris <christopher_hauss@gmx.de>
2014-04-04 07:08:56 +0200

    finished movefromto methode

--- modules/controller/movement.py
+++ modules/controller/movement.py
@@ -3,6 +3,7 @@ Created on 29.03.2014
 
 @author: Chris
 '''
+from modules.model.myExceptions import IllegalMoveError
 import logging.config
 
 logging.config.fileConfig('C:\\Users\\Chris\\git\\stratgame\\config\\log.config')
@@ -24,16 +25,23 @@ class Movement():
         movingToken = self.__playingfield.getTokenOnField(oldX, oldY)
         if not movingToken:
             raise ValueError("There is no token on field %d/%d to move" % (oldX, oldY))
+        elif player != movingToken.getOwner():
+            raise IllegalMoveError("Player %s tried to move with a token of player %s" % (player.getName(), movingToken.getOwner().getName()))
                              
         destiToken = self.__playingfield.getTokenOnField(newX, newY)
-        if destiToken:
+        if destiToken: #if there is a token on the field movingToken wants to move
+            
+            #delete the movingToken if its rank is lower than the destiToken
             if destiToken.getRank() > movingToken.getRank():
                 self.__playingfield.leaveField(oldX, oldY)
-                #TODO: 
+                player.removeTokenOnField(movingToken)
                 return
+            #else remove the destiToken and move to its position
             else:
                 self.__playingfield.leaveField(newX, newY)
                 self.__playingfield.leaveField(oldX, oldY)
+                destiToken.getOwner().removeTokenOnField(destiToken)
+                
                 
         self.__playingfield.placeToken(movingToken, newX, newY)
                 
