67ee41114858ae7743d7b112795d12b851419b11
Chris <christopher_hauss@gmx.de>
2014-04-04 21:30:18 +0200

    implemented the main while loop

--- modules/controller/consoleReader.py
+++ modules/controller/consoleReader.py
@@ -7,3 +7,47 @@ import logging.config
 
 logging.config.fileConfig('C:\\Users\\Chris\\git\\stratgame\\config\\log.config')
 logger = logging.getLogger('controller')
+tui = logging.getLogger('tui')
+
+class ConsoleReader(object):
+    '''
+    this class should be started in an extra thread.
+    it waits for input from the textualuserinterface
+    '''
+    def __init__(self, gameData, playingField):
+        self.__gameData = gameData
+        self.__playingField = playingField
+        logger.debug("Starting ConsoleReader...")
+        self._run()
+        
+    def _run(self):
+        while True:
+            myInput = raw_input()
+            logger.debug("Read the input: %s" % myInput)
+            if myInput[0] != '/':
+                tui.info("Commands begin with a \"/\"")
+                continue
+            
+            myInput = myInput.split()
+            if myInput[0] in ["/place", "/p"]:
+                self._tryToPlaceToken(myInput)
+            elif myInput[0] in ["/move", "/m"]:
+                self._tryToMoveToken(myInput)
+            else:
+                tui.info("Not a legal command: %s" % myInput[0])
+                
+                
+    def _tryToPlaceToken(self, myInput):
+        '''
+        tries to place a token if myInput has legal values
+        '''
+        
+    def _tryToMoveToken(self, myInput):
+        
+                
+            
+            
+            
+            
+            
+            
